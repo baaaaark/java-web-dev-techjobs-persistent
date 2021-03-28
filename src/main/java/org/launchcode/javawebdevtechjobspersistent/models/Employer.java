0d86e7fe25980e632952ead4d66dc1d3d4cec866
baaaaark <baaaaark@gmail.com>
2021-03-28 13:50:36 -0500

    initial commit up to 2.Data Layers

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,30 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @Size(min = 3, max = 128)
+    private String location;
 
+    public Employer() {
+    }
+
+    public Employer(String location) {
+    this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,27 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank
+    @Size(min = 7, max = 255)
+    public String skillDescription;
+
+    public Skill() {}
+
+    public Skill(String skillDescription) {
+        this.skillDescription = skillDescription;
+    }
+
+    public String getSkillDescription() {
+        return skillDescription;
+    }
+
+    public void setSkillDescription(String skillDescription) {
+        this.skillDescription = skillDescription;
+    }
 }
